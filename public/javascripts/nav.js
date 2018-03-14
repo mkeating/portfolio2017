bd0f17ba6c01fb5e96d583eb73a7fc1d272101a4
Michael <mkeating2225@gmail.com>
2018-03-13 23:40:52 -0400

    mobile nav finished

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -2,13 +2,17 @@
 window.onload = () => {
 
 	const mobileNavButton = document.querySelector('.mobile-nav-button');
-
+	const nav =document.querySelector('.nav');
+	
 	let menuUp = false;
 	mobileNavButton.addEventListener('click', (e) =>{
 		if(!menuUp){
 			console.log('deploying menu');
+		
+			nav.classList.add('deployed');
 		} else {
 			console.log('putting menu away');
+			nav.classList.remove('deployed');
 		}
 
 		menuUp = !menuUp;
